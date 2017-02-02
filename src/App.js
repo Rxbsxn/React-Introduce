b3898bbe1728f1b969cfcf45164c494d0396e02b
Robert Harężlak <harezlak.robert7@gmail.com>
2017-02-02 14:41:32 +0100

    control react component updates when new props are received

--- src/App.js
+++ src/App.js
@@ -4,48 +4,34 @@ import ReactDOM from 'react-dom';
 class App extends Component {
     constructor() {
         super();
-        this.state = {val: 0}
-        this.update = this.update.bind(this)
+        this.state = {increasing: false}
     }
+
     update() {
-        this.setState({val: this.state.val + 1})
-    }
-    componentWillMount() {
-        console.log('componentWillMount')
-        this.setState({m: 2})
-    }
-   
-    render() {
-        return <button onClick={this.update}>{ this.state.val * this.state.m }</button>
+        ReactDOM.render(<App val={this.props.val + 1 } />, document.getElementById('root')) 
     }
 
-     componentDidMount(){
-        console.log('componentDidMount')
-        // console.log(ReactDOM.findDOMNode(this))
-        this.inc = setInterval(this.update, 500);
+    componentWillReceiveProps(nextProps) {
+        this.setState({increasing: nextProps.val > this.props.val})
     }
-    componentWillUnmount() {
-        console.log('componentWillUnmount')
-        clearInterval(this.inc)
-    }
-}
 
-class Wrapper extends Component {
-    mount() {
-        ReactDOM.render(<App />, document.getElementById('a'))
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.val % 5 === 0;
     }
-    unmount() {
-        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
-    }
+
     render() {
+        console.log(this.state.increasing)
         return (
-            <div>
-                <button onClick={this.mount.bind(this)}>Mount </button>
-                <button onClick={this.unmount.bind(this)}>UnMount</button>
-                <div id="a"></div>
-            </div>
+            <button onClick={this.update.bind(this)}>
+                {this.props.val}
+            </button>
         )
     }
-}
+    componentDidUpdate(prevProps, prevState) {
+        console.log(`prevProps: ${prevProps.val}`)
+    }
+} 
 
-export default Wrapper
+App.defaultProps = {val : 0}
+
+export default App
